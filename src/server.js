b61353935d3b581584bce672e40827c6410301e5
Refri Rifwan <refririfwan@gmail.com>
2022-04-15 10:36:17 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -2,14 +2,18 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const albums = require('./api/albums');
 const songs = require('./api/songs');
+const users = require('./api/users');
 const AlbumsService = require('./services/postgres/AlbumsService');
 const SongsService = require('./services/postgres/SongsService');
+const UsersService = require('./services/postgres/UsersService');
 const AlbumsValidator = require('./validator/albums');
 const SongsValidator = require('./validator/songs');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -36,6 +40,15 @@ const init = async () => {
       validator: SongsValidator,
     },
   });
+
+  await server.register({
+    plugin: users,
+    options: {
+      service: usersService,
+      validator: UsersValidator,
+    },
+  });
+  
 
   await server.start();
   // eslint-disable-next-line no-console
