cd55cecc831e64d4f6c94df329aaffb4ac5770a2
Refri Rifwan <refririfwan@gmail.com>
2022-04-24 14:38:18 +0700

    Use Authentications Plugin on Hapi Server

--- src/server.js
+++ src/server.js
@@ -1,14 +1,19 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 const albums = require('./api/albums');
 const songs = require('./api/songs');
 const users = require('./api/users');
+const authentications = require('./api/authentications');
 const AlbumsService = require('./services/postgres/AlbumsService');
 const SongsService = require('./services/postgres/SongsService');
 const UsersService = require('./services/postgres/UsersService');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
 const AlbumsValidator = require('./validator/albums');
 const SongsValidator = require('./validator/songs');
 const UsersValidator = require('./validator/users');
+const AuthenticationsValidator = require('./validator/authentications');
 
 const init = async () => {
   const albumsService = new AlbumsService();
@@ -22,6 +27,28 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  server.auth.strategy('openmusicapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await server.register({
@@ -45,6 +72,16 @@ const init = async () => {
     options: {
       service: UsersService,
       validator: UsersValidator,
+    },
+  });
+
+  await server.register({
+    plugin: authentications,
+    options: {
+      AuthenticationsService,
+      UsersService,
+      tokenManager: TokenManager,
+      validator: AuthenticationsValidator,
     },
   });
 
