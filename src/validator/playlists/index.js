009b82e0f3381388b3ed9aef60070f55366bee5b
Refri Rifwan <refririfwan@gmail.com>
2022-05-21 13:29:16 +0700

    Cleaning Code Typo and Add FK albumid

--- src/validator/playlists/index.js
+++ src/validator/playlists/index.js
@@ -2,7 +2,7 @@ const InvariantError = require('../../exceptions/InvariantError');
 const { PlaylistPayloadSchema } = require('./schema');
 
 const PlaylistsValidator = {
-  validateAlbumPayload: (payload) => {
+  validatePlaylistPayload: (payload) => {
     const validationResult = PlaylistPayloadSchema.validate(payload);
     if (validationResult.error) {
       throw new InvariantError(validationResult.error.message);
